898cbcf95674fa6ad38c1b3840a5981d6b0ab260
YuliyaYaremchuk <118767028+jujalei@users.noreply.github.com>
2023-06-20 20:23:15 +0300

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section';
+import { Notification } from './Notification';
+
+import { FeedbackBTN } from './button/FeedbackBtn';
+import { Statistics } from './statistic/Statistic';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  updateFeedback = nameSt => {
+    this.setState(prevState => ({
+      [nameSt]: prevState[nameSt] + 1,
+    }));
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100) || 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const nameBtn = Object.keys(this.state);
+    const total = this.countTotalFeedback();
+    return (
+      <>
+        <Section title={'Please leave feedback'}>
+          <FeedbackBTN
+            options={nameBtn}
+            onLeaveFeedback={this.updateFeedback}
+          />
+        </Section>
+
+        <Section title={'Statistics'}>
+          {total <= 0 ? (
+            <Notification message="There is no feedback"></Notification>
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/button/FeedbackBTN.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Btn = styled.button`
+  cursor: pointer;
+  padding: 15px;
+  margin-right: 15px;
+  text-transform: capitalize;
+  font-size: 16px;
+  background-color: #ea8697;
+  border: none;
+  border-radius: 20px;
+  &:hover {
+    box-shadow: 4px 6px 37px -8px rgba(0, 0, 0, 0.75);
+  }
+`;
